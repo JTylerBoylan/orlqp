dac43880a3f1457d9e2a91b1a17d5eb8242fbdf4
Jonathan Boylan <jtylerboylan@outlook.com>
2023-10-20 13:21:54 -0400

    Add orlqp.hpp

---
+++ include/orlqp/orlqp.hpp
@@ -0,0 +1,10 @@
+#ifndef ORLQP_ORLQP_HPP_
+#define ORLQP_ORLQP_HPP_
+
+#include "orlqp/types.hpp"
+#include "orlqp/OSQP.hpp"
+#include "orlqp/QPProblem.hpp"
+#include "orlqp/QPArrayProblem.hpp"
+#include "orlqp/MPCProblem.hpp"
+
+#endif
